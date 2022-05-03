3913b36bd4e84cb0f1c6dcbcd2f86805db56dcf8
Tracey Bullington <traceylbullington@gmail.com>
2022-05-02 21:26:03 -0500

    first commit complete iteration 0, static to dos

---
+++ ToDoTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ToDoTableViewController.swift
+//  static to-dos
+//
+//  Created by Tracey Bullington on 5/2/22.
+//  Copyright © 2022 Tracey Bullington. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+var toDos : [ToDo] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   toDos = createToDos()
+    }
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
+    // MARK: - Table view data source
+
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+        // #warning Incomplete implementation, return the number of rows
+    }
+
+ 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+
+        if toDo.important {
+          cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+          cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+   
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ static to-dos/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  static to-dos
+//
+//  Created by Tracey Bullington on 5/2/22.
+//  Copyright © 2022 Tracey Bullington. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+  var name = ""
+  var important = false
+}
